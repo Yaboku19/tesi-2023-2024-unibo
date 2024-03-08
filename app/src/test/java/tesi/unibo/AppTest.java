c354e6aec70998a33b0f51437fc9bce29f461eb8
Yaboku19 <emanuele.martelli5@studio.unibo.it>
2024-03-08 13:40:29 +0100

    removed a warning

--- app/src/test/java/tesi/unibo/AppTest.java
+++ app/src/test/java/tesi/unibo/AppTest.java
@@ -4,7 +4,7 @@
 package tesi.unibo;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+//import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
     @Test void appHasAGreeting() {
