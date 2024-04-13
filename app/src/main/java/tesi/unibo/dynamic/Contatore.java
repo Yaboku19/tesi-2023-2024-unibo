d149e507e0b4a1782c6a2a80c40462cdc1944eae
Yaboku19 <emanuele.martelli5@studio.unibo.it>
2024-04-13 11:20:37 +0200

    removed an unused import

--- app/src/main/java/tesi/unibo/dynamic/Contatore.java
+++ app/src/main/java/tesi/unibo/dynamic/Contatore.java
@@ -2,17 +2,21 @@
 package tesi.unibo.dynamic;
 
 public class Contatore {
-    private int valore = 0;
-    
-    public void incrementa() {
-        valore++;
-    }
-    
-    public void decrementa() {
-        valore--;
-    }
-    
-    public int getValore() {
-        return valore;
-    }
+	private int valore;
+
+	public Contatore() {
+		this.valore = 0;
+	}
+
+	public void incrementa() {
+		this.valore++;
+	}
+
+	public void decrementa() {
+		this.valore--;
+	}
+
+	public int getValore() {
+		return this.valore;
+	}
 }
--- app/src/test/java/tesi/unibo/dynamic/DynamicTest.java
+++ app/src/test/java/tesi/unibo/dynamic/DynamicTest.java
@@ -1,5 +1,4 @@
 package tesi.unibo.dynamic;
-import tesi.unibo.dynamic.Contatore;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
